e4e8283c69bfed1ed74eee94b6af37bb9eb61fae
Ryan Low <ryanlwk99@gmail.com>
2022-09-22 17:17:53 +0800

    Created SignUp Fields

--- lib/screens/login/signup_screen.dart
+++ lib/screens/login/signup_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({ Key? key }) : super(key: key);
+  const SignUpScreen({Key? key}) : super(key: key);
   static const routeName = '/signup';
 
   @override
@@ -9,7 +9,54 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Sign Up", style: TextStyle(fontSize: 20),),
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        heightFactor: 2,
+        child: Form(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey[100]),
+                  child: TextFormField(
+                      decoration:
+                          InputDecoration(filled: true, labelText: "Name")),
+                  width: 350,
+                  height: 70,
+                ),
+                SizedBox(height: 16),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey[100]),
+                  width: 350,
+                  height: 70,
+                  child: TextFormField(
+                      decoration:
+                          InputDecoration(filled: true, labelText: "Email")),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey[100]),
+                  child: TextFormField(
+                      decoration:
+                          InputDecoration(filled: true, labelText: "Password")),
+                  width: 350,
+                  height: 70,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

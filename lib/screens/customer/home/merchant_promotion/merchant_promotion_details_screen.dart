93bd88a78ff5a8367d9cca64adb4ab77ae1fc74a
soonann <soonann.dev@gmail.com>
2022-11-07 01:49:00 +0800

    fixed quest claiming bug

--- lib/screens/customer/home/merchant_promotion/merchant_promotion_details_screen.dart
+++ lib/screens/customer/home/merchant_promotion/merchant_promotion_details_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:plantngo_frontend/models/promotion.dart';
 import 'package:share_plus/share_plus.dart';
 
 class MerchantPromotionDetailsScreen extends StatefulWidget {
-  String merchantName;
-  String merchantPromotionImage;
+  Promotion promotion;
 
   MerchantPromotionDetailsScreen({
     super.key,
-    required this.merchantName,
-    required this.merchantPromotionImage,
+    required this.promotion,
   });
 
   @override
@@ -27,62 +26,87 @@ class _MerchantPromotionDetailsScreenState
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
             SliverAppBar(
+              leading: BackButton(color: Colors.white),
+              title: Text(
+                "Promotion",
+              ),
               actions: [
                 Padding(
                   padding: const EdgeInsets.all(10),
                   child: IconButton(
                     onPressed: () {
-                      Share.share(widget.merchantPromotionImage);
+                      Share.share(widget.promotion.bannerUrl!);
                     },
                     icon: const Icon(
                       Icons.share,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ],
+
               floating: true,
-              flexibleSpace: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Colors.green.shade200,
-                      Colors.green.shade300,
-                      Colors.green,
-                    ],
-                  ),
-                ),
-                child: FlexibleSpaceBar(
-                  title: Text(
-                    widget.merchantName,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-              ),
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+
+              // flexibleSpace: Container(
+              //   decoration: BoxDecoration(
+              //     gradient: LinearGradient(
+              //       begin: Alignment.topLeft,
+              //       end: Alignment.bottomRight,
+              //       colors: [
+              //         Colors.green.shade200,
+              //         Colors.green.shade300,
+              //         Colors.green,
+              //       ],
+              //     ),
+              //   ),
+              //   child: FlexibleSpaceBar(
+              //     title: Text(
+              //       widget.merchantName,
+              //       style: Theme.of(context).textTheme.headline6,
+              //     ),
+              //   ),
+              // ),
             ),
           ];
         },
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.network(widget.merchantPromotionImage),
-            SizedBox(
-              height: 50,
-            ),
-            FractionallySizedBox(
-              widthFactor: 0.5,
-              child: ElevatedButton(
-                child: Text(
-                  "Claim Now!",
-                  style: Theme.of(context).textTheme.button,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(widget.promotion.bannerUrl!),
+              SizedBox(
+                height: 50,
               ),
-            )
-          ],
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: Text(
+                  widget.promotion.description!,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption!.copyWith(),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              FractionallySizedBox(
+                widthFactor: 0.5,
+                child: ElevatedButton(
+                  child: const Text(
+                    "Claim Now!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      backgroundColor: Colors.green,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
